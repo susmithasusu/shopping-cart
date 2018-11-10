82fbf28df56481ce4c8d8bcf0c8e0e6361178631
susmitha <susmithavs49@gmail.com>
2018-11-09 16:54:54 -0800

    product update changed

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -195,7 +195,7 @@ class ProductController extends RestController
         $image = str_replace(' ', '+', $image);
         $data = base64_decode($image);
         $imgName='img_'.$this->request['name'] .'.'.'png';
-        $new_img=Yii::$app->urlManager->createAbsoluteUrl("uploads").'/'.$this->request['image'];
+        $new_img=Yii::$app->urlManager->createAbsoluteUrl("uploads").'/'. $imgName;
         file_put_contents(\Yii::$app->basePath.'/web/uploads/'.$imgName, $data);
         exec('sudo chmod ' .Yii::$app->basePath.'/web/uploads/'.$imgName.'777');
 
